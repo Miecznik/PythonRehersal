544b394dbe03cd173c977cfb3909236e2f0fc9ba
Miecznik <45920036+Miecznik@users.noreply.github.com>
2023-03-10 20:51:46 +0100

    Test repozytorium

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+from datetime import datetime
+from math import pi
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -12,5 +15,21 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    print("""
+    Twinkle, twinkle, little star, 
+        How I wonder what you are!
+            Up above the world so high,
+            Like a diamond in the sky. 
+        Twinkle, twinkle, little star, 
+            How I wonder what you are!""")
+    print(sys.version)
+    print(sys.version_info)
+    teraz = datetime.now()
+    print("Obecna data i czas")
+    data = teraz.strftime("%Y/%M/%D" + "  " + "%H:%M:%S")
+    print(data)
+    r = float(input("Podaj r kola"))
+    print("obwod kola wynosi " + str(pi*r**2))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Początek
